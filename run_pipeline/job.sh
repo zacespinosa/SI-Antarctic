9b9978ddff4410a89683d3862a966e4e810cb6a5
zacespinosa <zespinosa97@gmail.com>
2023-12-05 16:15:45 -0700

    transform ensemble members

--- run_pipeline/job.sh
+++ run_pipeline/job.sh
@@ -1,11 +1,14 @@
 #!/bin/bash -l
 #PBS -N ensemble
 #PBS -A UWAS0118
-#PBS -q economy@chadmin1.ib0.cheyenne.ucar.edu
+#PBS -q casper
 #PBS -l walltime=12:00:00
 #PBS -l select=1:ncpus=1:mpiprocs=1
+#PBS -o run_transformer.o
+#PBS -e run_transformer.e
 
 module load conda
 conda activate cenv
 
-python /glade/work/zespinosa/Projects/SI-Antarctic/run_pipeline/run_dataloader.py
+#python /glade/work/zespinosa/Projects/SI-Antarctic/run_pipeline/run_dataloader.py
+python /glade/work/zespinosa/Projects/SI-Antarctic/run_pipeline/run_datatransformer.py
--- run_pipeline/run_datatransformer.py
+++ run_pipeline/run_datatransformer.py
@@ -20,7 +20,7 @@ REF_PERIOD = ("1980-01-01", "2020-01-01")
 TESTING = False
 SAVE = True
 
-def transform_cesm2_data(ds, save, save_name):
+def transform_cesm2_data(datatransformer, ds, save, save_name):
     print("starting regrid")
     ds = datatransformer.regrid(
         ds,
@@ -44,7 +44,7 @@ def transform_cesm2_data(ds, save, save_name):
         save_name=save_name
     )
 
-def transform_era5_data(era5_data, test, save, cvar):
+def transform_era5_data(datatransformer, era5_data, test, save, cvar):
 
     if test:
         era5_data = era5_data.sel(time=slice("1979-01-01", "1980-01-01"))
@@ -69,18 +69,6 @@ def transform_era5_data(era5_data, test, save, cvar):
         save_name=f"ERA5_monthly_1979-01_2023-12_{cvar}",
     )
 
-dataloader = DataLoader(
-    root = [
-        "/glade/campaign/univ/uwas0118/scratch/archive/1950_2015/",
-        "/glade/scratch/zespinosa/archive/cesm2.1.3_BHISTcmip6_f09_g17_ERA5_nudge/",
-        "/glade/scratch/zespinosa/archive/cesm2.1.3_BSSP370cmip6_f09_g17_ERA5_nudge/"
-    ],
-    era5_root="/glade/work/zespinosa/data/era5/monthly"
-)
-
-datatransformer = DataTransformer(
-    save_path='/glade/work/zespinosa/Projects/SI-Antarctic/data'
-)
 
 
 ##### ERA5 #####
@@ -102,38 +90,134 @@ datatransformer = DataTransformer(
 # transform_era5_data(era5_single, test=False, save=True, cvar="full-single")
 # print("Starting pressure level")
 # transform_era5_data(era5_pressure, test=False, save=True, cvar="full-pressure")
+########################## CESM2 Persistence Ensemble ####################################
+def transform_cesm2_ens():
+    dataloader = DataLoader(
+        root = [
+            "/glade/campaign/univ/uwas0118/scratch/archive/1950_2015/",
+            "/glade/scratch/zespinosa/archive/cesm2.1.3_BHISTcmip6_f09_g17_ERA5_nudge/",
+            "/glade/scratch/zespinosa/archive/cesm2.1.3_BSSP370cmip6_f09_g17_ERA5_nudge/"
+        ],
+        era5_root="/glade/work/zespinosa/data/era5/monthly"
+    )
+
+    members = ["1980", "1989", "1994", "1998", "1999", "2005", "2007", "2014", "2015", "2018"]
+    for ens_mem in members:
+        print("Starting member: ", ens_mem)
+        dataloader_ens = DataLoader(
+            root = [
+                f"/glade/scratch/zespinosa/archive/{ens_mem}_cesm2.1.3_BSSP370cmip6_f09_g17_ERA5_nudge"
+            ],
+            era5_root="/glade/work/zespinosa/data/era5/monthly"
+        )
+
+        datatransformer = DataTransformer(
+            save_path=f'/glade/work/zespinosa/Projects/SI-Antarctic/data/persistence_ensemble/{ens_mem}',
+        )
+
+        ##### ICE #####
+        print("starting ice")
+        ice_cesm2 = dataloader.get_cesm2_data(
+            comp="ice",
+            myvars=["aice", "daidtt", "daidtd", "dvidtt", "dvidtd", "sithick", "uvel", "vvel"],
+            testing=TESTING,
+        )
+        ice_cesm2_enso = dataloader_ens.get_cesm2_data(
+            comp="ice",
+            myvars=["aice", "daidtt", "daidtd", "dvidtt", "dvidtd", "sithick", "uvel", "vvel"],
+            testing=TESTING,
+        )
+        # Merge ENSO and NO ENSO
+        ice_cesm2 = xr.concat([
+            ice_cesm2.sel(time=slice("1950-01-15", "2022-12-15")),
+            ice_cesm2_enso,
+        ], dim="time")
+
+        transform_cesm2_data(
+            datatransformer=datatransformer,
+            ds=ice_cesm2,
+            save=SAVE,
+            save_name=f"{ens_mem}_cesm2_ice_monthly_1950-01_2023-12",
+        )
+
+        ##### OCN SST #####
+        print("starting ocn sst")
+        ocn_sst = dataloader.get_cesm2_data(comp="ocn", myvars=["SST"], testing=TESTING)
+        ocn_sst_enso = dataloader_ens.get_cesm2_data(comp="ocn", myvars=["SST"], testing=TESTING)
+        # Merge ENSO and NO ENSO
+        ocn_sst = xr.concat([
+            ocn_sst.sel(time=slice("1950-01-15", "2022-12-15")),
+            ocn_sst_enso,
+        ], dim="time")
+
+        transform_cesm2_data(
+            datatransformer=datatransformer,
+            ds=ocn_sst,
+            save=SAVE,
+            save_name=f"{ens_mem}_cesm2_ocn-sst_monthly_1950-01_2023-12",
+        )
+
+        ##### ATM #####
+        print("starting atm")
+        atm_cesm2 = dataloader.get_cesm2_data(
+            comp="atm",
+            myvars=["PSL", "U10", "TS", "T", "U", "V", "Z3"],
+            levels=[1000, 850, 500],
+            testing=TESTING
+        )
+        atm_cesm2_enso = dataloader_ens.get_cesm2_data(
+            comp="atm",
+            myvars=["PSL", "U10", "TS", "T", "U", "V", "Z3"],
+            levels=[1000, 850, 500],
+            testing=TESTING
+        )
+        # Merge ENSO and NO ENSO
+        atm_cesm2 = xr.concat([
+            atm_cesm2.sel(time=slice("1950-01-15", "2022-12-15")),
+            atm_cesm2_enso,
+        ], dim="time")
+
+        transform_cesm2_data(
+            datatransformer=datatransformer,
+            ds=atm_cesm2,
+            save=SAVE,
+            save_name=f"{ens_mem}_cesm2_atm_monthly_1950-01_2023-12",
+        )
+
+transform_cesm2_ens()
+
 
 ########################## CESM2 NO ENSO ####################################
-dataloader_enso = DataLoader(
-    root = [
-        "/glade/scratch/zespinosa/archive/NO_ENSO_cesm2.1.3_BSSP370cmip6_f09_g17_ERA5_nudge"
-    ],
-    era5_root="/glade/work/zespinosa/data/era5/monthly"
-)
+# dataloader_enso = DataLoader(
+#     root = [
+#         "/glade/scratch/zespinosa/archive/NO_ENSO_cesm2.1.3_BSSP370cmip6_f09_g17_ERA5_nudge"
+#     ],
+#     era5_root="/glade/work/zespinosa/data/era5/monthly"
+# )
 
-##### ICE #####
-print("starting ice")
-ice_cesm2 = dataloader.get_cesm2_data(
-    comp="ice",
-    myvars=["aice", "daidtt", "daidtd", "dvidtt", "dvidtd", "sithick", "uvel", "vvel"],
-    testing=TESTING,
-)
-ice_cesm2_enso = dataloader_enso.get_cesm2_data(
-    comp="ice",
-    myvars=["aice", "daidtt", "daidtd", "dvidtt", "dvidtd", "sithick", "uvel", "vvel"],
-    testing=TESTING,
-)
-# Merge ENSO and NO ENSO
-ice_cesm2 = xr.concat([
-    ice_cesm2.sel(time=slice("1950-01-15", "2022-12-15")),
-    ice_cesm2_enso,
-], dim="time")
+# ##### ICE #####
+# print("starting ice")
+# ice_cesm2 = dataloader.get_cesm2_data(
+#     comp="ice",
+#     myvars=["aice", "daidtt", "daidtd", "dvidtt", "dvidtd", "sithick", "uvel", "vvel"],
+#     testing=TESTING,
+# )
+# ice_cesm2_enso = dataloader_enso.get_cesm2_data(
+#     comp="ice",
+#     myvars=["aice", "daidtt", "daidtd", "dvidtt", "dvidtd", "sithick", "uvel", "vvel"],
+#     testing=TESTING,
+# )
+# # Merge ENSO and NO ENSO
+# ice_cesm2 = xr.concat([
+#     ice_cesm2.sel(time=slice("1950-01-15", "2022-12-15")),
+#     ice_cesm2_enso,
+# ], dim="time")
 
-transform_cesm2_data(
-    ds=ice_cesm2,
-    save=SAVE,
-    save_name="enso_cesm2_ice_monthly_1950-01_2023-12",
-)
+# transform_cesm2_data(
+#     ds=ice_cesm2,
+#     save=SAVE,
+#     save_name="enso_cesm2_ice_monthly_1950-01_2023-12",
+# )
 
 ##### OCN MXL #####
 # print("starting ocn mxl")
@@ -144,47 +228,47 @@ transform_cesm2_data(
 #     save_name="cesm2_ocn-mxl_monthly_1950-01_2023-12",
 # )
 
-##### OCN SST #####
-print("starting ocn sst")
-ocn_sst = dataloader.get_cesm2_data(comp="ocn", myvars=["SST"], testing=TESTING)
-ocn_sst_enso = dataloader_enso.get_cesm2_data(comp="ocn", myvars=["SST"], testing=TESTING)
-# Merge ENSO and NO ENSO
-ocn_sst = xr.concat([
-    ocn_sst.sel(time=slice("1950-01-15", "2022-12-15")),
-    ocn_sst_enso,
-], dim="time")
+# ##### OCN SST #####
+# print("starting ocn sst")
+# ocn_sst = dataloader.get_cesm2_data(comp="ocn", myvars=["SST"], testing=TESTING)
+# ocn_sst_enso = dataloader_enso.get_cesm2_data(comp="ocn", myvars=["SST"], testing=TESTING)
+# # Merge ENSO and NO ENSO
+# ocn_sst = xr.concat([
+#     ocn_sst.sel(time=slice("1950-01-15", "2022-12-15")),
+#     ocn_sst_enso,
+# ], dim="time")
 
-transform_cesm2_data(
-    ds=ocn_sst,
-    save=SAVE,
-    save_name="enso_cesm2_ocn-sst_monthly_1950-01_2023-12",
-)
+# transform_cesm2_data(
+#     ds=ocn_sst,
+#     save=SAVE,
+#     save_name="enso_cesm2_ocn-sst_monthly_1950-01_2023-12",
+# )
 
 ##### ATM #####
-print("starting atm")
-atm_cesm2 = dataloader.get_cesm2_data(
-    comp="atm",
-    myvars=["PSL", "U10", "TS", "T", "U", "V", "Z3"],
-    levels=[1000, 850, 500],
-    testing=TESTING
-)
-atm_cesm2_enso = dataloader_enso.get_cesm2_data(
-    comp="atm",
-    myvars=["PSL", "U10", "TS", "T", "U", "V", "Z3"],
-    levels=[1000, 850, 500],
-    testing=TESTING
-)
-# Merge ENSO and NO ENSO
-atm_cesm2 = xr.concat([
-    atm_cesm2.sel(time=slice("1950-01-15", "2022-12-15")),
-    atm_cesm2_enso,
-], dim="time")
+# print("starting atm")
+# atm_cesm2 = dataloader.get_cesm2_data(
+#     comp="atm",
+#     myvars=["PSL", "U10", "TS", "T", "U", "V", "Z3"],
+#     levels=[1000, 850, 500],
+#     testing=TESTING
+# )
+# atm_cesm2_enso = dataloader_enso.get_cesm2_data(
+#     comp="atm",
+#     myvars=["PSL", "U10", "TS", "T", "U", "V", "Z3"],
+#     levels=[1000, 850, 500],
+#     testing=TESTING
+# )
+# # Merge ENSO and NO ENSO
+# atm_cesm2 = xr.concat([
+#     atm_cesm2.sel(time=slice("1950-01-15", "2022-12-15")),
+#     atm_cesm2_enso,
+# ], dim="time")
 
-transform_cesm2_data(
-    ds=atm_cesm2,
-    save=SAVE,
-    save_name="enso_cesm2_atm_monthly_1950-01_2023-12",
-)
+# transform_cesm2_data(
+#     ds=atm_cesm2,
+#     save=SAVE,
+#     save_name="enso_cesm2_atm_monthly_1950-01_2023-12",
+# )
 
 
 ########################## CESM2 ####################################
